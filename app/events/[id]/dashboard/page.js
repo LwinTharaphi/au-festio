0362c6e5a241af1817a0e4b74de5ba03cf2b4363
MichelleThaeMyat <u6520209@au.edu>
2024-11-28 19:13:16 +0700

    Update redirect event list

--- app/events/[id]/dashboard/page.js
+++ app/events/[id]/dashboard/page.js
@@ -103,7 +103,7 @@ export default function Dashboard() {
         {/* Main Content */}
         <Col xs={10} className="main-content p-4">
           <Breadcrumb>
-            <Breadcrumb.Item href="/create-event">Create Events</Breadcrumb.Item>
+            <Breadcrumb.Item href="/organizers/[id]/create-event">Event List</Breadcrumb.Item>
             <Breadcrumb.Item href="/events">Events</Breadcrumb.Item>
             <Breadcrumb.Item active>{event.eventName}</Breadcrumb.Item>
           </Breadcrumb>
