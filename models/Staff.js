df16e5d417413d3d8c2dedd09c3df1175932789e
AeliaWin <u6540064@au.edu>
2025-01-28 21:43:33 +0700

    updated staff model

--- models/Staff.js
+++ models/Staff.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const staffSchema = new mongoose.Schema({
   id: { type: String, required: true },
+  firebaseUID: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
