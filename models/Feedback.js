5526264832124ba1c1ef25e20695b4b47a46fade
AeliaWin <u6540064@au.edu>
2025-01-28 17:09:03 +0700

    update feedback model

--- models/Feedback.js
+++ models/Feedback.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const feedbackSchema = new mongoose.Schema(
   {
     eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
+    firebaseUID: { type: String, required: true },
     stars: { type: Number, required: true, min: 1, max: 5 }, // Ratings from 1 to 5
     suggestion: { type: String, required: true, maxlength: 500 }, // Feedback text with a max length
   },
