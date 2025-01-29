009b792422b36fc6444204b972d015a6ffa9299f
AeliaWin <u6540064@au.edu>
2025-01-29 22:03:52 +0700

    update getCheckInqr api

--- app/api/organizers/[organizer_id]/events/[id]/students/getcheckinqr/route.js
+++ app/api/organizers/[organizer_id]/events/[id]/students/getcheckinqr/route.js
@@ -1,6 +1,7 @@
 import dbConnect from "@/lib/db";
 import QRCode from "@/models/QRcode";
 import Event from "@/models/Event";
+import Student from "@/models/Student";
 
 // GET: Retrieve the QR code for a specific event
 export async function GET(request, { params }) {
@@ -8,6 +9,14 @@ export async function GET(request, { params }) {
     await dbConnect(); // Connect to the database
 
     const { organizer_id, id } = params; // Get eventId from URL parameters
+    const { studentId, firebaseUID } = request.query; // Get studentId and firebaseUID from query parameters
+
+    if (!studentId || !firebaseUID) {
+      return new Response(
+        JSON.stringify({ message: 'Missing required parameters' }),
+        { status: 400 }
+      );
+    }
 
     const event = await Event.findOne({ _id: id, organizer: organizer_id });
 
@@ -18,7 +27,16 @@ export async function GET(request, { params }) {
       );
     }
 
-    const qrCode = await QRCode.findOne({ eventId: id });
+    const student = await Student.findOne({ _id: studentId, firebaseUID });
+
+    if (!student) {
+      return new Response(
+        JSON.stringify({ message: 'Student not found' }),
+        { status: 404 }
+      );
+    }
+
+    const qrCode = await QRCode.findOne({ studentId: studentId,eventId: id });
 
     if (!qrCode) {
       return new Response(
